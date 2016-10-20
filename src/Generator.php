2529efccf026b3aefa1a31f18c38e74f6dadbca8
Jan Skrasek <hrach.cz@gmail.com>
2016-10-20 09:29:35 +0200

    generator: added support for resources

--- src/Generator.php
+++ src/Generator.php
@@ -14,13 +14,21 @@ use Latte\Runtime\Html;
 
 class Generator
 {
-	public function generate($api)
+	public function generate($apiSpecification)
 	{
 		$latte = new Engine();
 		$latte->setTempDirectory(__DIR__ . '/../temp');
 		$latte->addFilter('urlvars', function ($url) {
 			return new Html(preg_replace('#(\{[\w_]+\})#', '<span class="url-var">\1</span>', $url));
 		});
-		return $latte->renderToString(__DIR__ . '/templates/content.latte', ['api' => $api]);
+		return $latte->renderToString(__DIR__ . '/templates/content.latte', [
+			'api' => $apiSpecification,
+			'loadReference' => function ($name) use ($apiSpecification) {
+				if (!isset($apiSpecification['resources'][$name])) {
+					throw new \RuntimeException("References $name schema not found.");
+				}
+				return $apiSpecification['resources'][$name];
+			},
+		]);
 	}
 }
